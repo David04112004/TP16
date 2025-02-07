8d7720138c6c374f008c262e316dd40deee2b044
David04112004 <david.azoulay@edu.ece.fr>
2025-02-07 10:02:52 +0100

    le version deux fonctionne

--- main.c
+++ main.c
@@ -1,3 +1,43 @@
 //
 // Created by david on 07/02/2025.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void allouerTableauEntiersV2(int** pTab, int const taille) {
+
+    *pTab = calloc(taille,sizeof(int));
+}
+
+
+int main() {
+    int* p = NULL;
+    int taille =4;
+
+     allouerTableauEntiersV2(&p,taille);
+
+    for(int i = 0 ; i< taille ;i++) {
+        printf("%d",p[i]);
+    }
+
+
+    free(p);
+    p = NULL;
+    return 0;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
